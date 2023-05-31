7cea88351a724ce24f233b404f28c478d3ca0674
Štěpán Velínský <stteranv@gmail.com>
2023-06-01 01:27:24 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte číslo k umocnění:");
+            int mocCislo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Výsledek je: "+mocCislo * mocCislo);
+            Console.ReadKey();
+
+        }
+    }
+}
